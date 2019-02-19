ff97c00f5b3233b5ae3a4a3617bc0da3279d7750
Terkwood <metaterkhorn@gmail.com>
2019-02-19 17:08:21 -0500

    Setup github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
