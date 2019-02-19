85fd948b7a6c2a16d3d4bccb61f2ae0f03cabbdd
Terkwood <metaterkhorn@gmail.com>
2019-02-19 16:56:18 -0500

    Update github pages

---
+++ docs/static/media/App.806e6cd9.rs
@@ -0,0 +1,17 @@
+#[no_mangle]
+pub extern "C" fn fact(mut n: u32) -> u32 {
+    let mut result = 1;
+    while n > 0 {
+        result = result * n;
+        n = n - 1;
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(super::fact(6), 720);
+    }
+}
